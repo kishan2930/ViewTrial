364e38bf75265cccffdc899434ba16509c9d9a30
Kishan Ambaliya <kishambaliya@gmail.com>
2025-05-23 16:05:03 +0530

    Chore: Edit rules

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -5,12 +5,12 @@ const Configuration: UserConfig = {
   rules: {
     'type-enum': [2, 'always', ['Feat', 'Fix', 'Chore']],
     'type-case': [2, 'always', 'pascal-case'],
-    'subject-case': [2, 'always', 'pascal-case'],
     'subject-empty': [2, 'never'],
+    'subject-case': [2, 'always', ['sentence-case']],
   },
   parserPreset: {
     parserOpts: {
-      headerPattern: /^(\w+): (.+)$/,
+      headerPattern: /^(Feat|Fix|Chore): (.+)$/,
       headerCorrespondence: ['type', 'subject'],
     },
   },
