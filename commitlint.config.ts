879a703874f22479422f437313b28acdb911a833
Kishan Ambaliya <kishambaliya@gmail.com>
2025-05-23 14:32:19 +0530

    Chore: add rules

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -5,14 +5,6 @@ const Configuration: UserConfig = {
   rules: {
     'type-enum': [2, 'always', ['Feat', 'Fix', 'Chore']],
     'type-case': [2, 'always', 'pascal-case'], // PascalCase: Feat, Fix, Chore
-    'subject-case': [0], // Allow any case in subject
-    'header-max-length': [2, 'always', 100],
-  },
-  parserPreset: {
-    parserOpts: {
-      headerPattern: /^(\w+): (.+)$/,
-      headerCorrespondence: ['type', 'subject'],
-    },
   },
 };
 
