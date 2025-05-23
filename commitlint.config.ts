3e561531c5b3c40a930bd059698c791e1845270a
Kishan Ambaliya <kishambaliya@gmail.com>
2025-05-23 14:30:29 +0530

    Chore: try to commit

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,6 +1,7 @@
 import type {UserConfig} from '@commitlint/types';
 
 const Configuration: UserConfig = {
+  extends: ['@commitlint/config-conventional'],
   rules: {
     'type-enum': [2, 'always', ['Feat', 'Fix', 'Chore']],
     'type-case': [2, 'always', 'pascal-case'], // PascalCase: Feat, Fix, Chore
