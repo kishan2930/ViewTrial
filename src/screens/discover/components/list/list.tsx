682577896517d2da96fa67cf28980d1d292cdfd5
Kishan Ambaliya <kishambaliya@gmail.com>
2025-05-26 12:03:04 +0530

    Chore: Fonts added

--- src/screens/discover/components/list/list.tsx
+++ src/screens/discover/components/list/list.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import {Text} from 'react-native';
 import ListItem from './components/listItem/listItem';
-import SkeletonLoading from './components/skeletonLoading/skeletonLoading';
 // import httpService from '@utils/httpService';
 // interface LocationResultItem {
 //   id: string;
@@ -55,11 +54,6 @@ const List: React.FC = () => {
   return (
     <>
       <Text>List component</Text>
-
-      {[...Array(3)].map((_, i) => (
-        <SkeletonLoading key={i} />
-      ))}
-
       <ListItem />
     </>
   );
